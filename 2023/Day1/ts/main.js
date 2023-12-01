e80994115400370243aaacd4d2bc34a395e99fb9
Radvir <kovacstihamer500@gmail.com>
2023-12-01 17:40:51 +0100

    da1 part2 progress

--- 2023/Day1/ts/main.js
+++ 2023/Day1/ts/main.js
@@ -31,12 +31,13 @@ function part1(list) {
 
 function part2(list) {
     let result = 0;
-    let nums = ("one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
+    let nums = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     for (let i = 0; i < list.length; i++) {
         let num = "";
         let first_num = "";
         let first_num_index = 0;
         let first_string_index = 0;
+        let first_string_num = "";
 
         for (let j = 0; j < list[i].length; j++) {
             if (isInt(list[i][j])) {
@@ -48,22 +49,22 @@ function part2(list) {
             for (let l = 0; l < nums.length; l++) {
                 if (list[i].includes(nums[l])) {
                     if (0== first_string_index || l < first_string_index) {
-                        first_string_index = list[i].indexOf(nums[l]);
+                        first_string_index = list[i].indexOf(nums[l])+1;
+                        first_string_num = nums[l];
                     }
                 }
             }
         }
-        console.log(first_num_index + "-" + first_string_index);
-        if (first_num_index < first_string_index) {
+        if (first_num_index != 0 && first_num_index < first_string_index) {
             first_num = list[i][first_num_index];
-            console.log("firstnum: "+ first_num);
         } else {
-            first_num = first_string_index + 1;
+            first_num = first_string_num;
         }
 
         let second_num = "";
         let second_num_index = 0;
         let second_string_index = 0;
+        let second_string_num = "";
         for (let j = list[i].length - 1; j >= 0; j--) {
             if (isInt(list[i][j])) {
                 second_num_index = j;
@@ -74,15 +75,16 @@ function part2(list) {
             for (let l = list[i].length - 1; l >= 0; l--) {
                 if (list[i].includes(nums[l])) {
                     if (l > second_string_index || 0 == second_string_index) {
-                        second_string_index = list[i].indexOf(nums[l]);
+                        second_string_index = list[i].indexOf(nums[l])+1;
+                        second_string_num = nums[l];
                     }
                 }
             }
         }
-        if (second_num_index > second_string_index) {
+        if (second_string_index != 0 && second_num_index > second_string_index) {
             second_num = list[i][second_num_index];
         } else {
-            second_num = second_string_index + 1;
+            second_num = second_string_num;
         }
         console.log(`${first_num}`);
         result += parseInt(`${first_num}${second_num}`);
