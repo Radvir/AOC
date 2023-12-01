68318c6cca735ad58ef75c965a70a4125f906fcc
Radvir <kovacstihamer500@gmail.com>
2023-12-01 21:53:23 +0100

    day1 part2 almost done

--- 2023/Day1/ts/main.js
+++ 2023/Day1/ts/main.js
@@ -35,8 +35,8 @@ function part2(list) {
     for (let i = 0; i < list.length; i++) {
         let num = "";
         let first_num = "";
-        let first_num_index = 0;
-        let first_string_index = 0;
+        let first_num_index = -1;
+        let first_string_index = -1;
         let first_string_num = "";
 
         for (let j = 0; j < list[i].length; j++) {
@@ -45,25 +45,25 @@ function part2(list) {
                 break;
             }
         }
-        for (let k = 0; k < list[i].length; k++) {
             for (let l = 0; l < nums.length; l++) {
                 if (list[i].includes(nums[l])) {
-                    if (0== first_string_index || l < first_string_index) {
-                        first_string_index = list[i].indexOf(nums[l])+1;
-                        first_string_num = nums[l];
+                    if (-1== first_string_index || list[i].indexOf(nums[l]) < first_string_index) {
+                        first_string_index = list[i].indexOf(nums[l]);
+                        first_string_num = l+1;
                     }
                 }
             }
-        }
-        if (first_num_index != 0 && first_num_index < first_string_index) {
+        
+
+        if (first_num_index != -1 && first_string_index != -1&& first_num_index < first_string_index) {
             first_num = list[i][first_num_index];
         } else {
             first_num = first_string_num;
         }
 
         let second_num = "";
-        let second_num_index = 0;
-        let second_string_index = 0;
+        let second_num_index = -1;
+        let second_string_index = -1;
         let second_string_num = "";
         for (let j = list[i].length - 1; j >= 0; j--) {
             if (isInt(list[i][j])) {
@@ -71,22 +71,21 @@ function part2(list) {
                 break;
             }
         }
-        for (let k = list[i].length - 1; k >= 0; k--) {
             for (let l = list[i].length - 1; l >= 0; l--) {
                 if (list[i].includes(nums[l])) {
-                    if (l > second_string_index || 0 == second_string_index) {
-                        second_string_index = list[i].indexOf(nums[l])+1;
-                        second_string_num = nums[l];
+                    if (list[i].indexOf(nums[l]) > second_string_index || -1 == second_string_index) {
+                        second_string_index = list[i].indexOf(nums[l]);
+                        second_string_num = l+1;
                     }
                 }
             }
-        }
-        if (second_string_index != 0 && second_num_index > second_string_index) {
+        
+        if (second_string_index != -1 && second_num_index != -1 &&second_num_index > second_string_index) {
             second_num = list[i][second_num_index];
         } else {
             second_num = second_string_num;
         }
-        console.log(`${first_num}`);
+        console.log(`${first_num}${second_num}`);
         result += parseInt(`${first_num}${second_num}`);
     }
     console.log(result)
