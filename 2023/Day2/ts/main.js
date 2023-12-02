f7b88c58a25e12ca6e691176619223558f2cc9e8
Radvir <kovacstihamer500@gmail.com>
2023-12-02 23:11:05 +0100

    started day2

---
+++ 2023/Day2/ts/main.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+let input = fs.readFileSync('input.txt', 'utf-8').split("\n");
+// console.log(data);
+let data;
+for (let i = 0; i < input.length; i++) {
+    let line_split = input[i].split(";");
+    let temp_data;
+    for (let i = 0; i < line_split.length; i++) {
+        temp_data.Add()
+    }
+}
+function part1(list) {
+    
+}
+
+function part2(list) {
+    
+}
+
+// part1(data);
+part2(data);
