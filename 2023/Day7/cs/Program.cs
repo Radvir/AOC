a2df0a5802d76935d5d7ae80fb5a6c80da6bfd74
Zsófia Széplaki <szeplaki.zsofia.21f@szlgbp.hu>
2023-12-07 13:41:36 +0100

    started day7

---
+++ 2023/Day7/cs/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Data.Common;
+using System.Globalization;
+using System.Data;
+
+namespace AOC_DayX
+{
+    public class Hand
+    {
+        public string cards;
+        public int bid;
+        public Hand(string cards, int bid)
+        {
+            this.cards = cards;
+            this.bid = bid;
+        }
+        public override string ToString() => $"{cards} {bid}";
+    }
+    public class Program
+    {
+        public static List<Hand> read()
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            List<Hand> list = new List<Hand>();
+            for (int i = 0; i < lines.Length; i++)
+                list.Add(new Hand(lines[i].Split(" ")[0], int.Parse(lines[i].Split(" ")[1])));
+            return list;
+        }
+        public static void part1(List<Hand> list)
+        {
+            
+        }
+
+        public static void part2(List<Hand> list)
+        {
+
+        }
+
+        public static void Main(string[] args)
+        {
+            List<Hand> input = read();
+            part1(input);
+            // part2(input);
+        }
+    }
+}
---
+++ 2023/templte/cs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Data.Common;
+using System.Globalization;
+using System.Data;
+
+namespace AOC_DayX
+{
+    public class Program
+    {
+        public static List<(long, long)> read()
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+            
+        }
+        public static void part1(List<(long, long)> list)
+        {
+            
+        }
+
+        public static void part2(List<(long, long)> list)
+        {
+            
+        }
+
+        public static void Main(string[] args)
+        {
+            List<(long, long)> input = read();
+            part1(input);
+            // part2(input);
+        }
+    }
+}
