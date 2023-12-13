15c4e33ba29ee2f06fa72d0346659d3f57600d77
Radvir <kovacstihamer500@gmail.com>
2023-12-13 20:31:30 +0100

    day10 p1 done

--- 2023/Day10/cs/Program.cs
+++ 2023/Day10/cs/Program.cs
@@ -8,31 +8,29 @@ using System.Runtime.CompilerServices;
 using System.Data.Common;
 using System.Globalization;
 using System.Data;
+using System.Xml.XPath;
 
 namespace AOC_DayX
 {
     public class Program
     {
-        public static (bool, string) isPipe(string s){
-            string pipes = "|-LJ7F";
-            if (pipes.Contains(s))
-                return (true, pipes[pipes.IndexOf(s.ToString())].ToString());
-            return (false, "");
-        }
         public static List<List<string>> read()
         {
             string[] lines = File.ReadAllLines("input.txt");
             List<List<string>> list = new List<List<string>>();
             for (int i = 0; i < lines.Length; i++)
             {
-                List<string> temp = new List<string>(lines[i].Split(""));
+                List<string> temp = new List<string>();
+                for (int j = 0; j < lines[i].Length; j++)
+                    temp.Add(lines[i][j].ToString());
                 list.Add(temp);
             }
             return list;
         }
         public static void part1(List<List<string>> list)
         {
-            (int, int) start_index = (0, 0);
+            int result = 1;
+            (int, int) start_index = (0, 0); //up&down --- left&right
             for (int i = 0; i < list.Count; i++)
             {
                 if (list[i].IndexOf("S") != -1)
@@ -42,12 +40,138 @@ namespace AOC_DayX
                 }
             }
 
-            //TODO: check the starting points surruindings
+            (int, int) curr = start_index;
+            for (int i = 0; i < 4; i++)
+            {
+                if (curr.Item1 > 0)
+                {
+                    string up = list[curr.Item1 - 1][curr.Item2];
+                    if ((up == "|" || up == "F" || up == "7")) //above
+                    {
+                        curr = (curr.Item1 - 1, curr.Item2);
+                        break;
+                    }
+                }
+                if (curr.Item1 < list.Count) //belove
+                {
+                    string down = list[curr.Item1 + 1][curr.Item2];
+                    if (down == "|" || down == "J" || down == "L")
+                    {
+                        curr = (curr.Item1 + 1, curr.Item2);
+                        break;
+                    }
+                }
+                if (curr.Item2 > 0) //left
+                {
+                    string left = list[curr.Item1][curr.Item2 - 1];
+                    if (left == "-" || left == "F" || left == "L")
+                    {
+                        curr = (curr.Item1, curr.Item2 - 1);
+                        break;
+                    }
+                }
+                if (curr.Item2 < list[0].Count) //right
+                {
+                    string right = list[curr.Item1][curr.Item2 + 1];
+                    if (right == "-" || right == "=" || right == "J")
+                    {
+                        curr = (curr.Item1, curr.Item2 + 1);
+                        break;
+                    }
+                }
+            }
+
+            (int, int) prev_curr = start_index;
+            while (curr != start_index)
+            {
+                string curr_value = list[curr.Item1][curr.Item2];
+                if (curr_value == "|")
+                {
+                    if (prev_curr.Item1 == curr.Item1 - 1)//pipe coming from up
+                    {
+                        prev_curr = curr;
+                        curr = (curr.Item1 + 1, curr.Item2);
+                    }
+                    else //pipe coming from down
+                    {
+                        prev_curr = curr;
+                        curr = (curr.Item1 - 1, curr.Item2);
+                    }
+                }
+                else if (curr_value == "-")
+                {
+                    if (prev_curr.Item2 == curr.Item2 - 1)//pipe is going right
+                    {
+                        prev_curr = curr;
+                        curr = (curr.Item1, curr.Item2 + 1);
+                    }
+                    else //pipe is going left
+                    {
+                        prev_curr = curr;
+                        curr = (curr.Item1, curr.Item2 - 1);
+                    }
+                }
+                else if (curr_value == "L")
+                {
+                    if (prev_curr.Item1 == curr.Item1 - 1)//pipe coming from up
+                    {
+                        prev_curr = curr;
+                        curr = (curr.Item1, curr.Item2 + 1);
+                    }
+                    else //pipe is going up
+                    {
+                        prev_curr = curr;
+                        curr = (curr.Item1 - 1, curr.Item2);
+                    }
+                }
+                else if (curr_value == "J")
+                {
+                    if (prev_curr.Item1 == curr.Item1 - 1)//pipe comes from up
+                    {
+                        prev_curr = curr;
+                        curr = (curr.Item1, curr.Item2 - 1); //going left
+                    }
+                    else //pipe is going up
+                    {
+                        prev_curr = curr;
+                        curr = (curr.Item1 - 1, curr.Item2);
+                    }
+                }
+                else if (curr_value == "7")
+                {
+                    if (prev_curr.Item1 == curr.Item1 + 1) //coming from down
+                    {
+                        prev_curr = curr;
+                        curr = (curr.Item1, curr.Item2 - 1);//going left
+                    }
+                    else //coming from left
+                    {
+                        prev_curr = curr;
+                        curr = (curr.Item1 + 1, curr.Item2);
+                    }
+                }
+                else if (curr_value == "F")
+                {
+                    if (prev_curr.Item1 == curr.Item1 + 1) //coming from down
+                    {
+                        prev_curr = curr;
+                        curr = (curr.Item1, curr.Item2 + 1);//going right
+                    }
+                    else //going down
+                    {
+                        prev_curr = curr;
+                        curr = (curr.Item1 + 1, curr.Item2);
+                    }
+                }
+                else System.Console.WriteLine("aalsékdfjlkéasdjfkléasdjfél");
+                result++;
+            }
+            System.Console.WriteLine(result/2);//TODO: divede it by 2
         }
 
         public static void part2(List<List<string>> list)
         {
-            
+
         }
 
         public static void Main(string[] args)
